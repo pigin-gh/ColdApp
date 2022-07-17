e0dcd33c5661454cbd1767c210d482ac2703a4fb
pigrist <pavel.pigin@gmail.com>
2022-07-17 18:04:52 +0300

    0.1.0 design

--- app/src/main/java/com/piginp/coldapp/DetailsScreen.kt
+++ app/src/main/java/com/piginp/coldapp/DetailsScreen.kt
@@ -3,9 +3,9 @@ package com.piginp.coldapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class DetailsScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_details_screen)
     }
 }
